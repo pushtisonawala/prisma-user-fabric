fcb371a33f4b0099cb091201df2fc0b99a85d9c5
Pushti Sonawala <pushtisonawala24@gmail.com>
2025-08-15 12:32:22 +0000

    first commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "john_doe",
+            password: "securepassword",
+            age: 30,
+            email: "john@example.com"
+        }
+    });
+}
+createUser();
+//# sourceMappingURL=index.js.map
---
+++ prisma/migrations/20250815111551_initialise/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "email" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
